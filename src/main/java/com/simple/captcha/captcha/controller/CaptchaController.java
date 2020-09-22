85b430948761cf1c28e9a3554cac1a549aeb8776
myungsung.jung <myungsung.jung@kurlycorp.com>
2020-09-22 13:00:45 +0900

    폴더구조를 변경하고 오류를 수정하라
    * 루트 폴더를 최상위로 변경하였습니다.
    * 캡차 이미지 생성 오류를 수정하였습니다.

---
+++ src/main/java/com/simple/captcha/captcha/controller/CaptchaController.java
@@ -0,0 +1,120 @@
+package com.simple.captcha.captcha.controller;
+
+import nl.captcha.Captcha;
+import nl.captcha.audio.AudioCaptcha;
+import nl.captcha.audio.producer.CustomNumberVoiceProducer;
+import nl.captcha.backgrounds.GradiatedBackgroundProducer;
+import nl.captcha.servlet.CaptchaServletUtil;
+import nl.captcha.text.producer.CustomTextProducer;
+import nl.captcha.text.producer.NumbersAnswerProducer;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CaptchaController {
+
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+  protected int _width = 200;
+
+  protected int _height = 50;
+
+  /**
+   * 캡차 생성
+   *
+   * @author 정명성
+   * @create date : 2016. 5. 20.
+   */
+  @RequestMapping(value = "/captcha")
+  public void setImages(HttpServletRequest request, HttpServletResponse response) {
+    Captcha captcha = new Captcha.Builder(_width, _height)
+        .addText(new NumbersAnswerProducer())
+        .addBackground(new GradiatedBackgroundProducer())
+        .gimp()
+        .addNoise()
+        .addBorder()
+        .build();
+
+    request.getSession().setAttribute("simpleCaptcha", captcha);
+    logger.info(request.getSession().getAttribute("simpleCaptcha").toString());
+    CaptchaServletUtil.writeImage(response, captcha.getImage());
+  }
+
+  /**
+   * 캡차로 생성된 내용을 가지고 오디오 및 정답을 만드는 방법
+   *
+   * @author 정명성
+   * @create date : 2016. 5. 20.
+   */
+  @RequestMapping(value = "/captchaAfterAudio")
+  public void playCaptchaAudio(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    HttpSession session = request.getSession(false);
+
+    Captcha captcha = (Captcha) session.getAttribute("simpleCaptcha");
+    CustomTextProducer customTextProducer = new CustomTextProducer(captcha.getAnswer().toCharArray());
+    CustomNumberVoiceProducer customNumberVoiceProducer = new CustomNumberVoiceProducer(captcha.getAnswer().toCharArray(), "kr");
+
+    AudioCaptcha ac = new AudioCaptcha.Builder()
+        .addAnswer(customTextProducer)
+        .addVoice(customNumberVoiceProducer)
+        .addNoise()
+        .build("kr");
+
+    request.getSession().setAttribute("simpleCaptchaAfterAudio", ac);
+    CaptchaServletUtil.writeAudio(response, ac.getChallenge());
+  }
+
+  /**
+   * 한글로 음성 캡차 만드는 방법
+   *
+   * @author 정명성
+   * @create date : 2016. 5. 20.
+   */
+  @RequestMapping(value = "/audio")
+  public void playAudio(HttpServletRequest request, HttpServletResponse response) {
+
+    AudioCaptcha ac = new AudioCaptcha.Builder()
+        .addAnswer()
+        .addNoise()
+        .build("kr");
+
+    request.getSession().setAttribute("simpleAudio", ac);
+    CaptchaServletUtil.writeAudio(response, ac.getChallenge());
+  }
+
+
+  /**
+   * 캡차 검증
+   *
+   * @author 정명성
+   * @create date : 2016. 5. 20.
+   */
+  @RequestMapping(value = "/check/captcha")
+  @ResponseBody
+  public String checkCaptcha(HttpServletRequest request) {
+    HttpSession session = request.getSession(false);
+
+    Captcha captcha = (Captcha) session.getAttribute("simpleCaptcha");
+    String answer;
+
+    if (request.getParameter("answer") == null) {
+      return "Wrong!";
+    } else {
+      answer = request.getParameter("answer");
+    }
+
+    if (captcha.isCorrect(answer)) {
+      return "Correct!";
+    } else {
+      return "Wrong!";
+    }
+  }
+
+}
---
+++ src/main/java/com/simple/captcha/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.simple.captcha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+  @RequestMapping(value = "/")
+  public String index() {
+    return "index";
+  }
+
+  @RequestMapping(value = "/sample")
+  public String sample() {
+    return "sample";
+  }
+
+}
